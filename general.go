f18fc7e37d0a2ea4412f3caa4e18cc4b24a6c436
codeblack24 <kostechofficial@gmail.com>
2025-12-07 20:03:09 +0100

    Update general.go

--- general.go
+++ general.go
@@ -1,27 +1,15 @@
-package piscine
+FUNCTION JumpOver(str):
 
-import (
-	"fmt"
+    IF length of str < 3 THEN
+        RETURN newline ("\n")
+    END IF
 
-	"github.com/01-edu/z01"
-)
+    result ← empty string
 
-func DealAPackOfCards(deck []int) {
-	for player := 0; player <= 4; player++ {
-		fmt.Printf("Player: ", player)
+    FOR i from 2 to length of str - 1, step 3 DO
+        result ← result + str[i]
+    END FOR
 
-		start := player * 4
-		end := start + 2
+    RETURN result + newline ("\n")
 
-		for i := start; i <= end; i++ {
-			if i == end+1 {
-				fmt.Printf("%d", deck[i])
-			} else {
-				fmt.Printf("%d, ", deck[start])
-			}
-		}
-
-		z01.PrintRune('\n')
-		z01.PrintRune('\n')
-	}
-}
+END FUNCTION

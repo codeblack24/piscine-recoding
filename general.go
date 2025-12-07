eb17f844b6e76e9e68c6ab4fd6b3b77a1ad77307
codeblack24 <kostechofficial@gmail.com>
2025-12-07 18:34:20 +0100

    Update general.go

--- general.go
+++ general.go
@@ -1,15 +1,24 @@
-FUNCTION Compact(list):
+FUNCTION Compact(pointer to list):
 
-    count = 0
+    Let s be the list we got from the pointer
 
-    FOR each item in the list:
-        IF item is NOT empty:
-            put item at position count
-            count = count + 1
+    Start a counter at 0
+
+    FOR each position i in the list:
+        IF the item at position i is NOT an empty string:
+            
+            Move this item to the front of the list
+            Specifically, put it at position equal to the counter
+
+            Increase the counter by 1
+
         END IF
     END FOR
 
-    cut the list so it keeps only the first 'count' items
+    Now remove everything after the counter position
+    (this cuts off all empty strings at the end)
 
-    RETURN count
+    Update the pointer so it points to this shorter list
+
+    RETURN the counter (the number of non-empty items)
 END FUNCTION
